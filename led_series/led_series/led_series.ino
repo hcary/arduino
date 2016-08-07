548fa0775c74eeb517a7730158cf90dfdbfe3153
Harvey Cary <hc1324@att.com>
2016-08-07 18:38:45 -0400

    Add Glow On & Off

--- led_series/led_series/led_series.ino
+++ led_series/led_series/led_series.ino
@@ -5,13 +5,13 @@
 // 8/2016
 
 const int delay_time1   = 50;     // Delay time
-const int delay_time2   = 10;
-const int blinkDelay    = 500;
-const int lowPin        = 5;     // Lowest pin used
-const int highPin       = 12;   // Highest pin used
-const int ledStart1     = 8;
-const int ledStart2     = 9;
-const int interation    = 2;
+const int delay_time2   = 10;     // Second delay time
+const int blinkDelay    = 500;    // Blink delay
+const int lowPin        = 5;      // Lowest pin used
+const int highPin       = 12;     // Highest pin used
+const int ledStart1     = 8;      // Middle low pin
+const int ledStart2     = 9;      // Middlw high pin
+const int interation    = 2;      // Number of times to loop through each sequence
 
 /************************************************/
 void setup()
@@ -29,6 +29,20 @@ void setup()
 void loop()
 {
 
+  for(int j = 1; j <= interation; j = j + 1)
+  {
+    for(int j = lowPin; j <= highPin; j = j + 1)
+    {
+      glowOn(j);
+    }
+  delay(1000);
+  
+    for(int j = lowPin; j <= highPin; j = j + 1)
+    {
+      glowOff(j);
+    }
+  }
+  
   allOff();
 
   for(int j = 1; j <= interation; j = j + 1)
@@ -63,6 +77,23 @@ void loop()
 }
 /*************************************************/
 
+void setPinAnalog()
+{
+
+  for(int x = lowPin; x <= highPin; x = x + 1){
+    pinMode(x,OUTPUT);
+  } 
+    
+}
+
+void setPinDigital()
+{
+
+  for(int x = lowPin; x <= highPin; x = x + 1){
+    pinMode(x,OUTPUT);
+  } 
+  
+}
 void pinSwitch( int pinNum, int level, int myDelay )
 {
 
@@ -70,6 +101,8 @@ void pinSwitch( int pinNum, int level, int myDelay )
   delay(myDelay);
   
 }
+
+
 
 void cylon()
 {
@@ -202,8 +235,38 @@ void insideOut()
     led2 = led2 - 1;      
   } // for(int x = 0; x <= 3; x = x + 1) 
 
-
 }
+
+void glowOn( int myPin )
+{
+
+  Serial.print(" Glow On ");
+  Serial.print(myPin);
+  Serial.println();
+  
+  for (int a=0; a<=255;a++)               //loop from 0 to 255
+  {
+    analogWrite(myPin, a);               // set the brightness of pin 9:
+    delay(5);                             //wait for 8 ms            
+  }
+
+  //delay(800); 
+}
+
+void glowOff( int myPin )
+{
+
+  Serial.print(" Glow Off ");
+  Serial.print(myPin);
+  Serial.println();
+  
+  for (int a=255; a>=0;a--)             //loop from 255 down to 0
+  {
+    analogWrite(myPin, a);               // set the brightness of pin 9:
+    delay(5);                             //wait for 8 ms  
+  }
+}
+
 
 
 
